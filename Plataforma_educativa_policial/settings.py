f0dc0ae7bac0a9bd4decb82d8f3fcb2cc02c1ce6
EduardoFelipe1106 <efelipep@uni.pe>
2022-09-25 18:14:44 -0500

    modified configuration

--- Plataforma_educativa_policial/settings.py
+++ Plataforma_educativa_policial/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
@@ -134,19 +133,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'Plataforma_educativa_policial/templates/assets',
+    'Plataforma_educativa_policial/templates/vendor',
+]
+
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    'Plataforma_educativa_policial/templates/assets',
-    'Plataforma_educativa_policial/templates/vendor',
-
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
